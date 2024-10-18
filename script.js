312e3ac16c4637d4d3f2ecca303a954818f43e5d
ngothuythanhtam <ntthanhtam.forwork@gmail.com>
2024-10-18 19:00:14 +0700

    Solved problems for changing theme option

--- script.js
+++ script.js
@@ -9,27 +9,12 @@ let currentGuess = [];
 let gameOver = false;
 
 // START FOR CHANGING THEME MODE
-let darkmode = localStorage.getItem('darkmode')
-const themeSwitch = document.getElementById('theme-switch');
-const themeEffect = document.querySelector('.theme-effect');
+let modeToggle = document.querySelector('.mode-tog');
+let darkMode = document.querySelector('.dark-mode');
 
-const enableDarkmode = () => {
-    document.body.classList.add('darkmode');
-    document.documentElement.classList.add('darkmode'); 
-    localStorage.setItem('darkmode', 'active');
-}
-
-const disableDarkmode = () => {
-    document.body.classList.remove('darkmode');
-    document.documentElement.classList.remove('darkmode'); 
-    localStorage.setItem('darkmode', null);
-}
-
-if(darkmode == "active") enableDarkmode()
-
-themeSwitch.addEventListener('click', function(){
-    darkmode = localStorage.getItem('darkmode')
-    darkmode!=="active" ? enableDarkmode() : disableDarkmode()
+modeToggle.addEventListener('click', () => {
+    darkMode.classList.toggle('active');
+    modeToggle.classList.toggle('active');
 })
 // END FOR CHANGING THEME MODE
 
